c45cd3a7e940dec887b870ee15eb68f9e9afe54a
Damar Sierra <damarsierra@gmail.com>
2023-09-20 10:20:29 -0400

    styled handles for slider

--- src/components/Slider/Handle/Handle.jsx
+++ src/components/Slider/Handle/Handle.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styles from './Handle.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 
 const Handle = ({type, onClick}) => {
   return (
     <>
       {type === "prev" ? 
         <span className={`${styles.handle} ${styles.prev}`} onClick={onClick}>
+          <FontAwesomeIcon icon={faChevronLeft} />
         </span>
         : 
         <span className={`${styles.handle} ${styles.next}`} onClick={onClick}>
+          <FontAwesomeIcon icon={faChevronRight} />
         </span>
       }
     </>
